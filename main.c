ef567e704763ef9225937058459e5507b101732e
Nikita Kolesnikov <nikolesnikov100@gmail.com>
2024-10-09 19:48:58 +0200

    LED and button

--- main.c
+++ main.c
@@ -1,25 +1,26 @@
 #include <stdint.h>
 #include <stm32f10x.h>
 
-void delay(uint32_t ticks) {
-	for (int i=0; i<ticks; i++) {
-		__NOP();
-	}
-}
 
 int __attribute((noreturn)) main(void) {
-	// Enable clock for AFIO
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+	//RCC->APB2ENR |= 0b10000;
+	// Включает тактирование PORTC, PORTB
+	RCC->APB2ENR |= (RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPBEN);
+	// Настройка PORTC, PC13 на выход, General purpose output push-pull
+	GPIOC->CRH &= ~(GPIO_CRH_CNF13 | GPIO_CRH_MODE13) | GPIO_CRH_MODE13_0; 
+	// Настройка PORTB, PB9 на вход с подтягивающим резистором
+	GPIOB->CRH &= ~(GPIO_CRH_CNF9 | GPIO_CRH_MODE9) | GPIO_CRH_CNF9_1;
+	// Включение pull-up на PB9
+	GPIOB->ODR |= GPIO_ODR_ODR9;
+	// Запись логического 0 на выход PC13 (LED ON)
+	GPIOC->ODR &= ~GPIO_ODR_ODR13;
 
     while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(1000000);
-	    GPIOC->ODR &= ~(1U<<13U);
-	    delay(1000000);
+		if(GPIOB->IDR & GPIO_IDR_IDR9){ 
+			GPIOC->ODR &= ~GPIO_ODR_ODR13; // LED ON
+		} else { // Button is pressed
+			GPIOC->ODR |= GPIO_ODR_ODR13; // LED OFF
+		}
     }
+	
 }
